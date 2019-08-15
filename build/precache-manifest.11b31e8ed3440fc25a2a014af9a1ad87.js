57453d342da8864db60e381830d5484684c79b7f
Sushovan Adhikari <Sushovan.Adhikari@ibm.com>
2019-08-15 00:25:12 -0500

    Todo React Crash Course
    A simple Todo app (for reference purpose) that shows important react functionality such as binding the child functions with the parent functions, use of state and props in parent and child components.

---
+++ build/precache-manifest.11b31e8ed3440fc25a2a014af9a1ad87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4ffdf8c55e1cd113ac3815dd385b91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b70c141735f4a0d75cc1",
+    "url": "/static/css/main.87a87563.chunk.css"
+  },
+  {
+    "revision": "158ceb21e337d7808287",
+    "url": "/static/js/2.2fb05ead.chunk.js"
+  },
+  {
+    "revision": "b70c141735f4a0d75cc1",
+    "url": "/static/js/main.0d088718.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+
+            <h1>About</h1>
+            <p>This is the TodoList app v1.0.0. It is a part of a React component</p>
+        </React.Fragment>
+    )
+}
